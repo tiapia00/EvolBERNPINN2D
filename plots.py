6ad589d2d11f8c82ceb77a6ec764939073e64574
tiapia00 <10680648@polimi.it>
2024-04-26 12:41:23 +0000

    Added plot_sol function

--- plots.py
+++ plots.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import MaxNLocator, FuncFormatter
 import torch
+import matplotlib.animation as animation
+from matplotlib import rc
+from pinn import f
 
 def plot_initial_conditions(z: torch.tensor, y: torch.tensor, x: torch.tensor, name: str, n_train: int, from_pinn: bool = True):
     """Plot initial conditions."""
@@ -57,3 +60,43 @@ def plot_initial_conditions(z: torch.tensor, y: torch.tensor, x: torch.tensor, n
     ax[1].set_box_aspect([2, 2, 1])
 
     fig.suptitle(name)
+    
+def plot_solution(pinn: PINN, x: torch.Tensor, y: torch.Tensor, t: torch.Tensor, n_train : int, figsize=(12, 8), dpi=100):
+
+    rc('animation', html='jshtml')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_zlim(0, 20)
+    
+    ax.set_xlabel('$\\hat{y}$')
+    ax.set_ylabel('$\\hat{x}$')
+    ax.set_title('$\\hat{u}_y(t)$')
+    ax.view_init(elev=30, azim=45)
+    
+    def animate(i):
+
+        if not i % 10 == 0:
+            ax.clear()
+            t_partial = torch.ones_like(x_raw) * t_raw[i]
+            f = f(pinn, x_raw, y_raw, t_partial)
+            uy_raw = f.detach().cpu().numpy()[:,1]
+            
+            x_raw = x.detach().cpu().numpy()
+            y_raw = y.detach().cpu().numpy()
+            
+            x_mesh = x_raw.reshape(n_train, n_train, n_train)
+            y_mesh = y_raw.reshape(n_train, n_train, n_train)
+            uy = uy_raw.reshape(n_train, n_train, n_train)
+            
+            ax.plot_surface(x_mesh[:,:,i], y_mesh[:,:,i], uy[:,:,i], label=f"Time {float(t_raw[i])}", cmap='viridis')
+            
+            return ax
+        
+    n_frames = t_raw.shape[0]
+    return animation.FuncAnimation(fig, animate, frames=n_frames, blit=False, repeat=True)
+    
+    
