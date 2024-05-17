6bf0dca50bc3cd20d4cd4960127a2a402ce78aae
tiapia00 <10680648@polimi.it>
2024-05-17 09:24:06 +0200

    updated formatting of pinn

--- pinn.py
+++ pinn.py
@@ -1,3 +1,8 @@
+from torch.utils.tensorboard import SummaryWriter
+import pytz
+import datetime
+from datetime import date
+from tqdm import tqdm
 from typing import Callable
 import numpy as np
 import torch
@@ -8,12 +13,14 @@ import os
 from read_write import pass_folder, get_current_time, get_last_modified_file, get_current_time, create_folder_date
 import matplotlib.pyplot as plt
 
-def initial_conditions(x: torch.tensor, y : torch.tensor, Lx: float, i: float = 1) -> torch.tensor:
+
+def initial_conditions(x: torch.tensor, y: torch.tensor, Lx: float, i: float = 1) -> torch.tensor:
     res_ux = torch.zeros_like(x)
     res_uy = torch.sin(torch.pi*i/x[-1]*x)
     return res_ux, res_uy
 
-def get_initial_points(x_domain, y_domain, t_domain, n_points, device = torch.device("cuda" if torch.cuda.is_available() else "cpu"), requires_grad=True):
+
+def get_initial_points(x_domain, y_domain, t_domain, n_points, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), requires_grad=True):
     x_linspace = torch.linspace(x_domain[0], x_domain[1], n_points)
     y_linspace = torch.linspace(y_domain[0], y_domain[1], n_points)
     x_grid, y_grid = torch.meshgrid(x_linspace, y_linspace, indexing="ij")
@@ -24,7 +31,8 @@ def get_initial_points(x_domain, y_domain, t_domain, n_points, device = torch.de
     t0 = torch.full_like(x_grid, t_domain[0], requires_grad=requires_grad)
     return (x_grid, y_grid, t0)
 
-def get_boundary_points(x_domain, y_domain, t_domain, n_points, device = torch.device("cuda" if torch.cuda.is_available() else "cpu"), requires_grad=True):
+
+def get_boundary_points(x_domain, y_domain, t_domain, n_points, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), requires_grad=True):
     """
          .+------+
        .' |    .'|
@@ -41,7 +49,7 @@ def get_boundary_points(x_domain, y_domain, t_domain, n_points, device = torch.d
     t_linspace = torch.linspace(t_domain[0], t_domain[1], n_points)
 
     x_grid, t_grid = torch.meshgrid(x_linspace, t_linspace, indexing="ij")
-    y_grid, _      = torch.meshgrid(y_linspace, t_linspace, indexing="ij")
+    y_grid, _ = torch.meshgrid(y_linspace, t_linspace, indexing="ij")
 
     x_grid = x_grid.reshape(-1, 1).to(device)
     x_grid.requires_grad = requires_grad
@@ -55,17 +63,21 @@ def get_boundary_points(x_domain, y_domain, t_domain, n_points, device = torch.d
     y0 = torch.full_like(t_grid, y_domain[0], requires_grad=requires_grad)
     y1 = torch.full_like(t_grid, y_domain[1], requires_grad=requires_grad)
 
-    down    = (y_grid, x0,     t_grid)
-    up      = (y_grid, x1,     t_grid)
-    left    = (y0,     x_grid, t_grid)
-    right   = (y1,     x_grid, t_grid)
+    down = (y_grid, x0,     t_grid)
+    up = (y_grid, x1,     t_grid)
+    left = (y0,     x_grid, t_grid)
+    right = (y1,     x_grid, t_grid)
 
     return down, up, left, right
 
-def get_interior_points(x_domain, y_domain, t_domain, n_points, device = torch.device("cuda" if torch.cuda.is_available() else "cpu"), requires_grad=True):
-    x_raw = torch.linspace(x_domain[0], x_domain[1], steps=n_points, requires_grad=requires_grad)
-    y_raw = torch.linspace(y_domain[0], y_domain[1], steps=n_points, requires_grad=requires_grad)
-    t_raw = torch.linspace(t_domain[0], t_domain[1], steps=n_points, requires_grad=requires_grad)
+
+def get_interior_points(x_domain, y_domain, t_domain, n_points, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), requires_grad=True):
+    x_raw = torch.linspace(
+        x_domain[0], x_domain[1], steps=n_points, requires_grad=requires_grad)
+    y_raw = torch.linspace(
+        y_domain[0], y_domain[1], steps=n_points, requires_grad=requires_grad)
+    t_raw = torch.linspace(
+        t_domain[0], t_domain[1], steps=n_points, requires_grad=requires_grad)
     grids = torch.meshgrid(x_raw, y_raw, t_raw, indexing="ij")
 
     x = grids[0].reshape(-1, 1).to(device)
@@ -74,13 +86,15 @@ def get_interior_points(x_domain, y_domain, t_domain, n_points, device = torch.d
 
     return x, y, t
 
+
 class PINN(nn.Module):
     """Simple neural network accepting two features as input and returning a single output
 
     In the context of PINNs, the neural network is used as universal function approximator
     to approximate the solution of the differential equation
     """
-    def __init__(self, num_hidden: int, dim_hidden: int, dim_input : int = 3, dim_output : int = 4, act=nn.Tanh()):
+
+    def __init__(self, num_hidden: int, dim_hidden: int, dim_input: int = 3, dim_output: int = 4, act=nn.Tanh()):
 
         super().__init__()
         self.dim_hidden = dim_hidden
@@ -101,7 +115,7 @@ class PINN(nn.Module):
     def forward(self, x, y, t):
         if x.dim() == 1:
             x_stack = torch.cat([x, y, t], dim=0)
-            x_stack = x_stack.reshape(1,-1)
+            x_stack = x_stack.reshape(1, -1)
         else:
             x_stack = torch.cat([x, y, t], dim=1)
         out = self.act(self.layer_in(x_stack))
@@ -117,12 +131,14 @@ class PINN(nn.Module):
     def device(self):
         return next(self.parameters()).device
 
+
 def f(pinn: PINN, x: torch.Tensor, y: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
     """Compute the value of the approximate solution from the NN model
     Internally calling the forward method when calling the class as a function"""
     return pinn(x, y, t)
 
-def df(output: torch.Tensor, inputs: list, var : int) -> torch.Tensor:
+
+def df(output: torch.Tensor, inputs: list, var: int) -> torch.Tensor:
     """Compute neural network derivative with respect to input features using PyTorch autograd engine
     var = 0 : dux
     var = 1 : duy
@@ -140,6 +156,7 @@ def df(output: torch.Tensor, inputs: list, var : int) -> torch.Tensor:
         )[0]
 
     return df_value
+
 
 class Loss:
     """Loss:
@@ -165,7 +182,8 @@ class Loss:
         self.initial_condition = initial_condition
 
     def residual_loss(self, pinn):
-        x, y, t = get_interior_points(self.x_domain, self.y_domain, self.t_domain, self.n_points, pinn.device())
+        x, y, t = get_interior_points(
+            self.x_domain, self.y_domain, self.t_domain, self.n_points, pinn.device())
         output = f(pinn, x, y, t)
 
         dvx_t = df(output, [t], 2)
@@ -179,15 +197,20 @@ class Loss:
         dux_xy = df(output, [x, y], 0)
         duy_xy = df(output, [x, y], 1)
 
-        loss1 = dvx_t - 2*self.z[0]*(dux_xx + 1/2*(dux_yy + duy_xy)) - self.z[1]*(dux_xx + duy_xy)
-        loss2 = dvy_t - 2*self.z[0]*(1/2*(duy_xx + dux_xy) + duy_yy) - self.z[1]*(dux_xy + duy_yy)
-        loss3 = dvx_t - df(output, [t,t], 0)
-        loss4 = dvy_t - df(output, [t,t], 1)
+        loss1 = dvx_t - 2 * \
+            self.z[0]*(dux_xx + 1/2*(dux_yy + duy_xy)) - \
+            self.z[1]*(dux_xx + duy_xy)
+        loss2 = dvy_t - 2 * \
+            self.z[0]*(1/2*(duy_xx + dux_xy) + duy_yy) - \
+            self.z[1]*(dux_xy + duy_yy)
+        loss3 = dvx_t - df(output, [t, t], 0)
+        loss4 = dvy_t - df(output, [t, t], 1)
 
         return (loss1.pow(2).mean() + loss2.pow(2).mean() + loss3.pow(2).mean() + loss4.pow(2).mean())
 
     def initial_loss(self, pinn, epochs):
-        x, y, t = get_initial_points(self.x_domain, self.y_domain, self.t_domain, self.n_points, pinn.device())
+        x, y, t = get_initial_points(
+            self.x_domain, self.y_domain, self.t_domain, self.n_points, pinn.device())
         pinn_init_ux, pinn_init_uy = self.initial_condition(x, y, x[-1])
         output = f(pinn, x, y, t)
 
@@ -197,11 +220,11 @@ class Loss:
                         y.cpu().detach().numpy()+pinn_init_uy.cpu().detach().numpy())
             plt.savefig('initial_cond.png')
 
-        ux = output[:, 0].reshape(-1,1)
-        uy = output[:, 1].reshape(-1,1)
+        ux = output[:, 0].reshape(-1, 1)
+        uy = output[:, 1].reshape(-1, 1)
 
-        vx = output[:, 2].reshape(-1,1)
-        vy = output[:, 3].reshape(-1,1)
+        vx = output[:, 2].reshape(-1, 1)
+        vy = output[:, 3].reshape(-1, 1)
 
         loss1 = ux - pinn_init_ux
         loss2 = uy - pinn_init_uy
@@ -212,7 +235,8 @@ class Loss:
         return (loss1.pow(2).mean() + loss2.pow(2).mean() + loss3.pow(2).mean() + loss4.pow(2).mean())
 
     def boundary_loss(self, pinn):
-        down, up, left, right = get_boundary_points(self.x_domain, self.y_domain, self.t_domain, self.n_points, pinn.device())
+        down, up, left, right = get_boundary_points(
+            self.x_domain, self.y_domain, self.t_domain, self.n_points, pinn.device())
         x_down, y_down, t_down = down
         x_up, y_up, t_up = up
         x_left, y_left, t_left = left
@@ -239,13 +263,13 @@ class Loss:
         dux_y_right = df(right, [y_right], 0)
         duy_x_right = df(right, [x_right], 1)
         tr_right = df(right, [x_right], 0) + duy_y_right
-        
+
         loss_upx = ux_up
         loss_upy = uy_up
-        
+
         loss_downx = ux_down
         loss_downy = uy_down
-        
+
         loss_left1 = 2*self.z[0]*(1/2*(dux_y_left + duy_x_left))
         loss_left2 = 2*self.z[0]*duy_y_left + self.z[1]*tr_left
 
@@ -253,7 +277,7 @@ class Loss:
         loss_right2 = 2*self.z[0]*duy_y_right + self.z[1]*tr_right
 
         return (loss_upx.pow(2).mean() + loss_upy.pow(2).mean() +
-                loss_downx.pow(2).mean() + loss_downy.pow(2).mean() + 
+                loss_downx.pow(2).mean() + loss_downy.pow(2).mean() +
                 loss_left1.pow(2).mean() + loss_left2.pow(2).mean() +
                 loss_right1.pow(2).mean() + loss_right2.pow(2).mean())
 
@@ -272,11 +296,6 @@ class Loss:
     def __call__(self, pinn, epoch):
         return self.verbose(pinn, epoch)[0]
 
-from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
-from datetime import date
-import datetime
-import pytz
 
 def train_model(
     nn_approximator: PINN,
@@ -287,11 +306,11 @@ def train_model(
 ) -> PINN:
 
     optimizer = optim.Adam([
-                        {'params': nn_approximator.layer_in.parameters()},
-                        {'params': nn_approximator.middle_layers.parameters()},
-                        {'params': nn_approximator.layer_out.parameters()},
-                        {'params': nn_approximator.weights, 'lr': -0.01},
-                        ], lr=learning_rate)
+        {'params': nn_approximator.layer_in.parameters()},
+        {'params': nn_approximator.middle_layers.parameters()},
+        {'params': nn_approximator.layer_out.parameters()},
+        {'params': nn_approximator.weights, 'lr': -0.01},
+    ], lr=learning_rate)
     loss_values = []
     loss: torch.Tensor = torch.inf
 
@@ -303,7 +322,8 @@ def train_model(
         grads = []
 
         optimizer.zero_grad()
-        _, residual_loss, initial_loss, boundary_loss = loss_fn.verbose(nn_approximator, epoch)
+        _, residual_loss, initial_loss, boundary_loss = loss_fn.verbose(
+            nn_approximator, epoch)
         loss: torch.Tensor = loss_fn(nn_approximator, epoch)
 
         loss.backward()
@@ -314,16 +334,16 @@ def train_model(
         pbar.set_description(f"Global loss: {loss.item():.2f}")
 
         writer.add_scalars(f'Loss', {
-                                    'global': loss.item(),
-                                    'residual': residual_loss.item(),
-                                    'initial': initial_loss.item(),
-                                    'boundary': boundary_loss.item(),
-                                    }, epoch)
+            'global': loss.item(),
+            'residual': residual_loss.item(),
+            'initial': initial_loss.item(),
+            'boundary': boundary_loss.item(),
+        }, epoch)
         writer.add_scalars(f'Weights', {
-                                    'residual': nn_approximator.weights[0].item(),
-                                    'initial': nn_approximator.weights[1].item(),
-                                    'boundary': nn_approximator.weights[2].item(),
-                                    }, epoch)
+            'residual': nn_approximator.weights[0].item(),
+            'initial': nn_approximator.weights[1].item(),
+            'boundary': nn_approximator.weights[2].item(),
+        }, epoch)
 
         pbar.update(1)
 
@@ -334,7 +354,8 @@ def train_model(
 
     return nn_approximator, np.array(loss_values)
 
-def return_adim(x_dom : np.ndarray, t_dom:np.ndarray, rho: float, mu : float, lam : float):
+
+def return_adim(x_dom: np.ndarray, t_dom: np.ndarray, rho: float, mu: float, lam: float):
     L_ast = x_dom[-1]
     T_ast = t_dom[-1]
     z_1 = T_ast**2/(L_ast*rho)*mu
