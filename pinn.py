cabc41b4ca38ab4fa1641e0e118901efd80c1e45
tiapia00 <10680648@polimi.it>
2024-05-22 09:44:47 +0000

    moved plots

--- pinn.py
+++ pinn.py
@@ -12,52 +12,6 @@ import torch.optim as optim
 from typing import Tuple
 import os
 from read_write import pass_folder, get_current_time, get_last_modified_file, get_current_time, create_folder_date
-import matplotlib.pyplot as plt
-from matplotlib.cm import viridis
-
-def scatter_penalty_loss2D(x: torch.tensor, y: torch.tensor, n_train: int, factors: torch.tensor):
-    x = x.reshape(n_train, n_train).detach().cpu().numpy()
-    y = y.reshape(n_train, n_train).detach().cpu().numpy()
-    factors = factors.reshape(n_train, n_train).detach().cpu().numpy()
-
-    fig = plt.figure()
-    plt.scatter(x, y, c=factors, cmap=viridis)
-    plt.colorbar()
-
-    plt.xlabel('x')
-    plt.ylabel('y')
-
-    fig.canvas.draw()
-
-    image_np = np.array(fig.canvas.renderer.buffer_rgba())
-    plt.close(fig)
-    image_tensor = torch.from_numpy(image_np).permute(2, 0, 1)
-
-    return image_tensor
-
-def scatter_penalty_loss3D(x: torch.tensor, y: torch.tensor, t: torch.tensor, n_train: int, factors: torch.tensor):
-    x = x.reshape(n_train, n_train, n_train).detach().cpu().numpy()
-    y = y.reshape(n_train, n_train, n_train).detach().cpu().numpy()
-    t = t.reshape(n_train, n_train, n_train).detach().cpu().numpy()
-    factors = factors.reshape(n_train, n_train, n_train).detach().cpu().numpy()
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    sc = ax.scatter(x, y, t, c=factors, cmap=viridis)
-
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('t')
-
-    cbar = fig.colorbar(sc, ax=ax)
-
-    fig.canvas.draw()
-
-    image_np = np.array(fig.canvas.renderer.buffer_rgba())
-    plt.close(fig)
-    image_tensor = torch.from_numpy(image_np).permute(2, 0, 1)
-
-    return image_tensor
 
 def initial_conditions(x: torch.tensor, y: torch.tensor, Lx: float, i: float = 1) -> torch.tensor:
     res_ux = torch.zeros_like(x)
@@ -378,7 +332,6 @@ class Loss:
     def __call__(self, pinn, epoch):
         return self.verbose(pinn, epoch)[0]
 
-
 def train_model(
     nn_approximator: PINN,
     loss_fn: Callable,
@@ -388,6 +341,8 @@ def train_model(
     points: dict,
     n_train: int
 ) -> PINN:
+    
+    from plots import scatter_penalty_loss2D, scatter_penalty_loss3D
 
     optimizer = optim.Adam([
         {'params': nn_approximator.layer_in.parameters()},
