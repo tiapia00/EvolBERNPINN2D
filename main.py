fixed normalization of analytical, fixed energy term

main.py
@@ -45,7 +45,7 @@ lam, mu = par.to_matpar_PINN()
 Lx, Ly, T, n_train, dim_hidden, lr, epochs = get_params(par.pinn_par)
 
 x_domain = np.array([0.0, Lx])/Lx
-y_domain = np.array([0.0, Ly])/Lx
+y_domain = np.array([-Ly/2, Ly/2])/Lx
 t_domain = np.array([0.0, T])/T
 
 grid = Grid(x_domain, y_domain, t_domain, n_train, device)
@@ -99,13 +99,14 @@ x = x.to(device)
 y = y.to(device)
 t = t.to(device)
 z = f(pinn_trained, x, y, t)
-ux0, uy0 = initial_conditions(x, y, Lx, i=1)
+ux0, uy0 = initial_conditions(x, y, i=1)
 z0 = torch.cat((ux0, uy0), dim=1)
 
 plot_initial_conditions(z, z0, x, y, n_train, dir_model)
 
 x, y, t = grid.get_interior_points()
 plot_sol(pinn_trained, x, y, t, n_train, dir_model, 'NN prediction', device)
+
 w_ad_mid = w_ad[:, int(w_ad.shape[0]/2)]
 plot_midpoint_displ(pinn_trained, t, n_train, w_ad_mid[1:], dir_model, device)
 plot_sol_comparison(pinn_trained, x, y, t, w_ad, n_train, dir_model, 'Comparison with analytical solution', device)

pinn.py
@@ -14,9 +14,10 @@ import os
 from read_write import pass_folder, get_current_time, get_last_modified_file, get_current_time, create_folder_date
 
 
-def initial_conditions(x: torch.tensor, y: torch.tensor, Lx: float, i: float = 1) -> torch.tensor:
+def initial_conditions(x: torch.tensor, y: torch.tensor, i: float = 1) -> torch.tensor:
+    A = 0.5
     res_ux = torch.zeros_like(x)
-    res_uy = 0.5*torch.sin(torch.pi*i/x[-1]*x)
+    res_uy = A*torch.sin(torch.pi*i/x[-1]*x)
     return res_ux, res_uy
 
 
@@ -221,7 +222,13 @@ class PINN(nn.Module):
         x_stack = torch.cat([x, y, t], dim=1)
         out = self.layer_in(x_stack)
         logits = self.layer_out(out)
-        return logits
+
+        hard_enc = torch.sin(x*np.pi)
+        hard_enc = hard_enc.view(-1, 1)
+        hard_enc_both = hard_enc.expand(hard_enc.shape[0], 4)
+        
+        out = logits*hard_enc_both
+        return out 
 
     def forward_mask(self, idx: int):
         masked_weights = torch.sigmoid(self.weights[idx])
@@ -232,10 +239,7 @@ class PINN(nn.Module):
 
 
 def f(pinn: PINN, x: torch.Tensor, y: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
-    # hard_enc = torch.sin(x*np.pi)
-    # hard_enc = hard_enc.view(-1, 1)
-    # hard_enc_both = hard_enc.expand(hard_enc.shape[0], 4)
-    return pinn(x, y, t)
+        return pinn(x, y, t)
 
 
 def df(output: torch.Tensor, inputs: list, var: int = 0) -> torch.Tensor:
@@ -305,7 +309,11 @@ class Loss:
         loss3 = m*(dvx_t - df(output, [t, t], 0))
         loss4 = m*(dvy_t - df(output, [t, t], 1))
 
-        d_en = (dvx_t + dvy_t) + self.z[0]*(dux_x + duy_y)**2 +\
+        vx = output[:,2].reshape(-1,1)
+        vy = output[:,3].reshape(-1,1)
+        h = torch.max(y) - torch.min(y)
+        
+        d_en = (1/2*h*(vx+vy)).pow(2) + self.z[0]*(dux_x + duy_y)**2 +\
             self.z[1]*2*(dux_x**2 + duy_y**2 + (dux_y+duy_x)
                          ** 2 + (duy_x + dux_y)**2)
 
@@ -368,11 +376,11 @@ class Loss:
         duy_x_right = df(right, [x_right], 1)
         tr_right = df(right, [x_right], 0) + duy_y_right
 
-        loss_upx = ux_up
-        loss_upy = uy_up
+ #       loss_upx = ux_up
+ #       loss_upy = uy_up
 
-        loss_downx = ux_down
-        loss_downy = uy_down
+ #       loss_downx = ux_down
+ #       loss_downy = uy_down
 
         loss_left1 = 2*self.z[0]*(1/2*(dux_y_left + duy_x_left))
         loss_left2 = 2*self.z[0]*duy_y_left + self.z[1]*tr_left
@@ -380,8 +388,8 @@ class Loss:
         loss_right1 = 2*self.z[0]*(1/2*(dux_y_right + duy_x_right))
         loss_right2 = 2*self.z[0]*duy_y_right + self.z[1]*tr_right
 
-        return pinn.forward_mask(2)*(loss_upx.pow(2).mean() + loss_upy.pow(2).mean() +
-                                     loss_downx.pow(2).mean() + loss_downy.pow(2).mean() +
+        return pinn.forward_mask(2)*(#loss_upx.pow(2).mean() + loss_upy.pow(2).mean() +
+                                     #loss_downx.pow(2).mean() + loss_downy.pow(2).mean() +
                                      loss_left1.pow(2).mean() + loss_left2.pow(2).mean() +
                                      loss_right1.pow(2).mean() + loss_right2.pow(2).mean())
 

plots.py
@@ -7,7 +7,6 @@ from matplotlib.animation import FuncAnimation
 from pinn import PINN, f
 import numpy as np
 
-
 def scatter_penalty_loss2D(x: torch.tensor, y: torch.tensor, n_train: int, factors: torch.tensor):
     x = x.reshape(n_train, n_train).detach().cpu().numpy()
     y = y.reshape(n_train, n_train).detach().cpu().numpy()
@@ -50,6 +49,7 @@ def scatter_penalty_loss3D(x: torch.tensor, y: torch.tensor, t: torch.tensor, n_
     cbar = fig.colorbar(sc, ax=ax)
 
     fig.canvas.draw()
+    plt.tight_layout()
 
     image_np = np.array(fig.canvas.renderer.buffer_rgba())
     plt.close(fig)
@@ -251,14 +251,14 @@ def plot_sol(pinn: PINN, x: torch.Tensor, y: torch.Tensor, t: torch.Tensor, n_tr
     ani.save(file, fps=60)
 
 
-def plot_midpoint_displ(pinn: PINN, t: torch.Tensor, n_train: int, t_ad: np.ndarray, uy_mid: np.ndarray, path: str, device):
+def plot_midpoint_displ(pinn: PINN, t: torch.Tensor, n_train: int, uy_mid: np.ndarray, path: str, device):
     fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10, 8))
     fig.suptitle('Midpoint displacement')
 
     t_raw = torch.unique(t, sorted=True)
 
     x = torch.tensor([0.5]).to(device).reshape(-1, 1)
-    y = torch.tensor([0.5]).to(device).reshape(-1, 1)
+    y = torch.tensor([0.]).to(device).reshape(-1, 1)
 
     uy_mid_PINN = []
 
@@ -274,7 +274,7 @@ def plot_midpoint_displ(pinn: PINN, t: torch.Tensor, n_train: int, t_ad: np.ndar
     ax[0].set_xlabel('$\\hat{t}$')
     ax[0].set_ylabel('$\\hat{u}_y$')
     
-    ax[1].plot(t_ad, uy_mid-np.array(uy_mid_PINN), color='red')
+    ax[1].plot(t_raw.cpu().detach().numpy(), uy_mid-np.array(uy_mid_PINN), color='red')
     ax[1].set_title('Deviation from analytical')
     ax[1].set_xlabel('$\\hat{t}$')
     ax[1].set_ylabel('$\\hat{u}_\\text{y,an}-\\hat{u}_\\text{y,PINN}$')