86a7323b2c94b3d3e76424e234f50740ea7550af
tiapia00 <10680648@polimi.it>
2024-06-03 08:44:22 +0000

    restored hard encoding, definitely yields better results

--- plots.py
+++ plots.py
@@ -291,7 +291,7 @@ def plot_sol(pinn: PINN, x: torch.Tensor, y: torch.Tensor, t: torch.Tensor, n_tr
             ax):
 
         x_limts = np.array([0, 2])
-        y_limts = np.array([-0.25, 0.25])
+        y_limts = np.array([-0.75, 0.75])
         t = t_shaped*t_raw[frame]
 
         output = f(pinn, x, y, t)
