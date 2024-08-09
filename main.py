introduced loss on derivative of energy, fixed caluclation of inenloss

main.py
@@ -20,10 +20,7 @@ else:
     device = torch.device("cpu")
     print("Using CPU device.")
 
-dir_model = pass_folder('model')
-dir_logs = pass_folder('model/logs')
-
-restartraining = True
+restartraining = False 
 
 def get_step(tensors: tuple):
     a, b, c = tensors
@@ -116,28 +113,26 @@ pinn = PINN(multdim, nax, ntrans, w0, nlayers, penalties).to(device)
 Psi_0, K_0 = calculate.gete0(pinn)
 
 model_name = f'{lr_formin}_{epochs}_{ntrans}.pth'
+
+dir_model = pass_folder('model')
+dir_logs = pass_folder('model/logs')
+
+model_name = f'{lr_formin}_{epochs}_{ntrans}.pth'
 model_path = os.path.join(dir_model, model_name)
 
 if restartraining:
     pinn_trained, ens_NN = train_model(pinn, calc=calculate, lr_formin=lr_formin,
             lr_formax=lr_formax, max_epochs=epochs, path_logs=dir_logs)
-
-    model_name = f'{lr_formin}_{epochs}_{ntrans}.pth'
-    model_path = os.path.join(dir_model, model_name)
-
     torch.save(pinn_trained.state_dict(), model_path)
 
 else:
     pinn_trained = PINN(multdim, nax, ntrans, w0, nlayers, penalties).to(device)
     ### Specify here filename ###
-    filename = '' 
-    dir_model = os.path.dirname(filename)
-    print(f'Target for outputs: {dir_model}\n')
-
+    filename = 'model//08-04//1714//0.001_2000_1.pth' 
     pinn_trained.load_state_dict(torch.load(filename, map_location=device))
     print(f'{filename} loaded.\n')
 
-    pinn_trained, ens_NN = train_model(pinn, calc=calculate, lr_formin=lr_formin,
+    pinn_trained, ens_NN = train_model(pinn_trained, calc=calculate, lr_formin=lr_formin,
             lr_formax=lr_formax, max_epochs=epochs, path_logs=dir_logs)
     torch.save(pinn_trained.state_dict(), model_path)
 

pinn.py
@@ -541,7 +541,13 @@ class Calculate:
         T = getkinetic(speed, nsamples, rho, (dx, dy)).reshape(-1)
 
         Pi0, T0 = self.gete0(pinn)
-        loss = applymask(pinn.penalty_en) * ((Pi0 + T0) - (Pi+T))
+
+        dPi = df_num_torch(dt, Pi)
+        dT = df_num_torch(dt, T)
+
+        loss = (Pi0 + T0) - (Pi+T)
+        loss += dPi + dT
+        loss *= applymask(pinn.penalty_en.squeeze(1))
 
         # Hamilton principle: action should be minimized
         if verbose:
@@ -594,9 +600,9 @@ class Calculate:
         gt = initial_conditions(x, self.w0)
         v0gt = gt[:,2:]
         v0 = getspeed(output, t, self.device)
-        loss_speed = (applymask(nn.penalty_in) * (v0gt - v0)).pow(2).mean()
+        loss_speed = (applymask(nn.penalty_in) * (v0gt - v0))
 
-        loss = loss_speed
+        loss = loss_speed.pow(2).mean()
 
         return loss
 
@@ -726,8 +732,9 @@ def calculate_speed(pinn_trained: PINN, points: tuple, device: torch.device) -> 
 
 def df_num_torch(dx: float, y: torch.tensor):
     dy = torch.diff(y)
+    device = y.device
 
-    derivative = torch.zeros_like(y)
+    derivative = torch.zeros_like(y, device=device)
 
     # Forward difference for the first point
     derivative[0] = dy[0] / dx

plots.py
@@ -116,8 +116,8 @@ def plot_energy(indicators_nn: dict, indicators_an: dict, t_nn: torch.tensor, t_
     plt.figure()
     plt.plot(t_nn, T_nn, label='$T_{{NN}}$')
     plt.plot(t_nn, Pi_nn, label='$\\Pi_{{NN}}$')
-    plt.plot(t_beam, T_an, label='$T_{{an}}$')
-    plt.plot(t_beam, Pi_an, label='$\\Pi_{{an}}$')
+    #plt.plot(t_beam, T_an, label='$T_{{an}}$')
+    #plt.plot(t_beam, Pi_an, label='$\\Pi_{{an}}$')
     plt.legend()
 
     file = f'{path}/energyfinal.png'