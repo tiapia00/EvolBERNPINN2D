36a09ec8dc4daaf9331be7f47e182559b8bb86ed
tiapia00 <10680648@polimi.it>
2024-08-12 20:16:54 +0200

    increased epochs

--- par.py
+++ par.py
@@ -22,7 +22,7 @@ class Parameters:
             'nlayers': self.nlayers,
             'lr_formin': 1e-3,
             'lr_formax': -1e-3,
-            'epochs': 1000 
+            'epochs': 8000 
         }
         self.beam_par = {
             'x_end': self.x_end,
