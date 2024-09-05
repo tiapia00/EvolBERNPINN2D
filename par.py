9c42fb514cdf5cd21b54d82c9a9fbe7f49e34689
tiapia00 <10680648@polimi.it>
2024-09-05 09:17:53 +0200

    increased epochs

--- par.py
+++ par.py
@@ -18,7 +18,7 @@ class Parameters:
             'dim_hidden': self.dim_hidden,
             'n_hidden_space': self.n_hidden,
             'lr': 1e-3,
-            'epochs': 2000
+            'epochs': 4000
         }
         self.beam_par = {
             'x_end': self.x_end,
