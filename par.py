77bef5eacda57db3e497f4ec7980b5bb904cffcf
tiapia00 <10680648@polimi.it>
2024-09-05 09:54:21 +0200

    increased dim_hidden and n_hidden

--- par.py
+++ par.py
@@ -5,7 +5,7 @@ class Parameters:
         self.t_end = 0.4
         self.n_space = 20
         self.n_time = 50
-        self.dim_hidden = (2, 20, 20)
+        self.dim_hidden = (1, 20, 20)
         self.n_hidden : int = 5
         self.w0 = 0.3
         self.pinn_par = {
