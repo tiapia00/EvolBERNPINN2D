75544ca053850833cba4155e5261ad2a0faf690a
tiapia00 <10680648@polimi.it>
2024-09-06 19:35:24 +0200

    rand initialization of Bs

--- pinn.py
+++ pinn.py
@@ -200,11 +200,11 @@ class PINN(nn.Module):
         self.n_mode_spacey = dim_hidden[1]
 
         multipliers_x = torch.arange(1, self.n_mode_spacex + 1, device=device)
-        self.Bx = 0.1 * torch.ones((2, self.n_mode_spacex), device=device)
+        self.Bx = 0.1 * torch.rand((2, self.n_mode_spacex), device=device)
         self.Bx[0,:] *= multipliers_x
 
         multipliers_y = torch.arange(1, self.n_mode_spacey + 1, device=device) **2
-        self.By = 0.005 * torch.ones((2, self.n_mode_spacey), device=device)
+        self.By = 0.005 * torch.rand((2, self.n_mode_spacey), device=device)
         self.By[0,:] *= multipliers_y
 
         self.in_time = nn.Linear(1, dim_hidden[2])
