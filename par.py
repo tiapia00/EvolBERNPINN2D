5b3b32dad6c76fdbe19b496ce6af274b02938215
tiapia00 <10680648@polimi.it>
2024-09-09 11:23:57 +0200

    reduced dimhidden

--- par.py
+++ par.py
@@ -5,7 +5,7 @@ class Parameters:
         self.t_end = 0.8
         self.n_space = 41 
         self.n_time = 61 
-        self.dim_hidden = (1, 20, 20)
+        self.dim_hidden = (1, 10, 10)
         self.n_hidden : int = 2 
         self.w0 = 0.3
         self.pinn_par = {
