4607a25884ee3f3414ac19526a3aaf8ee45ed4e6
tiapia00 <10680648@polimi.it>
2024-09-09 08:45:25 +0200

    odd number of samples

--- par.py
+++ par.py
@@ -3,9 +3,9 @@ class Parameters:
         self.x_end = 1 
         self.y_end = 1e-3
         self.t_end = 0.8
-        self.n_space = (41, 6)
+        self.n_space = (41, 7)
         self.n_modes = (1, 3)
-        self.n_time = 40 
+        self.n_time = 51 
         self.multdim = (2, 2) 
         self.nlayers = (1, 1) 
         self.w0 = 0.3
