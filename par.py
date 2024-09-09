8c7e11139e8aa11e0add302c431a9fd700c2f43d
tiapia00 <10680648@polimi.it>
2024-09-09 11:31:37 +0200

    increased hidden dim

--- par.py
+++ par.py
@@ -18,7 +18,7 @@ class Parameters:
             'dim_hidden': self.dim_hidden,
             'n_hidden_space': self.n_hidden,
             'lr': 1e-3,
-            'epochs': 3000
+            'epochs': 7000
         }
         self.beam_par = {
             'x_end': self.x_end,
@@ -29,14 +29,14 @@ class Parameters:
             'w0': self.w0
         }
         self.mat_par = {
-            'E': 68.0e9/1e6,
+            'E': 68.0e9,
             'rho': 7000,
             'nu': 0.26
         }
 
     def to_matpar_PINN(self) -> float:
 
-        E = self.mat_par['E']
+        E = self.mat_par['E']/1e6
         nu = self.mat_par['nu']
         lam = E*nu/(1+nu)/(1-2*nu)
         mu = E/2/(1+nu)
