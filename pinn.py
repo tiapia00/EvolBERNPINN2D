a4e69e5eeeed86e3ad4afe09cd3d4de0714e37d4
tiapia00 <10680648@polimi.it>
2024-09-09 12:40:29 +0200

    increased epochs for Adim

--- pinn.py
+++ pinn.py
@@ -634,7 +634,7 @@ def train_model(
 
         losses = (inen_loss, init_loss)
 
-        if epoch != 0 and epoch % 200 == 0 :
+        if epoch != 0 and epoch % 300 == 0 :
             pde_loss.backward()
             max_grad = get_max_grad(nn_approximator)
             optimizer.zero_grad()
