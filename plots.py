561c46ffa57d9e2264cad1f7c8d5d9d846fe92f5
tiapia00 <10680648@polimi.it>
2024-09-16 21:30:07 +0200

    scaled appropriately graph

--- plots.py
+++ plots.py
@@ -97,7 +97,7 @@ def plot_sol(sol: torch.Tensor, space_in: torch.Tensor, t: torch.Tensor, path: s
     ax.set_title(f'$\\hat{{t}} = {t[0]:.2f}$')
 
     def update(frame):
-        y_limts = np.array([-0.5, 0.5])
+        y_limts = np.array([torch.min(sol).item(), torch.max(sol).item()])
 
         ax.clear()
 
@@ -115,131 +115,6 @@ def plot_sol(sol: torch.Tensor, space_in: torch.Tensor, t: torch.Tensor, path: s
 
     file = f'{path}/sol_time.gif'
     ani.save(file, fps=5)
-
-
-def plot_sol_comparison(pinn: PINN, x: torch.Tensor, y: torch.Tensor, t: torch.Tensor, w_ad: np.ndarray,
-                        n_space: int, n_time: int, n_beam: int, path: str, device):
-
-    # y_plot squeezed for better visualization purposes, anyway is not encoded in the 1D solution, displacements not squeezed
-
-    nx = n_space
-    ny = n_space
-    nt = n_time
-
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 8))
-
-    t_raw = torch.unique(t, sorted=True)
-    t_raw = t_raw.reshape(-1, 1)
-
-    x_raw = x.reshape(nx, ny, nt)
-    y_raw = y.reshape(nx, ny, nt)
-
-    x = x_raw[:, :, 0]
-    y = y_raw[:, :, 0]
-
-    x = x.reshape(-1, 1).to(device)
-    y = y.reshape(-1, 1).to(device)
-
-    x_mid = torch.unique(x).reshape(-1, 1)
-    y_mid = torch.zeros_like(x_mid)
-
-    t_shaped = torch.ones_like(x).to(device)
-    t = t_shaped*t_raw[0].to(device)
-
-    output = f(pinn, x, y, t)
-
-    x_plot = x.cpu().detach().numpy().reshape(nx, ny).reshape(-1)
-    y_plot = 1/10*y.cpu().detach().numpy().reshape(nx, ny).reshape(-1)
-
-    z0 = output.cpu().detach().numpy()
-
-    sc = ax.scatter(x_plot+z0[:, 0], y_plot+z0[:, 1])
-    ax.scatter(np.unique(x_plot), w_ad[::int(n_beam/n_space),0])
-    t_value = float(t_raw[0])
-
-    ax.set_title(f'$\\hat{{t}} = {t_value:.2f}$')
-
-    def update(
-            frame,
-            x: torch.tensor,
-            y: torch.tensor,
-            x_mid: torch.tensor,
-            y_mid: torch.tensor,
-            n: int,
-            w_ad: np.ndarray,
-            x_plot: np.ndarray,
-            y_plot: np.ndarray,
-            t_raw: torch.tensor,
-            t_shaped: torch.tensor,
-            pinn: PINN,
-            ax):
-
-        x_limts = np.array([0, 2])
-        y_limts = np.array([-0.5, 0.5])
-        t = t_shaped*t_raw[frame].to(device)
-
-        output = f(pinn, x, y, t)
-
-        z = output.cpu().detach().numpy()
-
-        t_value = float(t[0])
-
-        ax.clear()
-
-        ax.set_xlabel('$\\hat{x}$')
-        ax.set_ylabel('$\\hat{y}$')
-        ax.set_title(f'$\\hat{{t}} = {t_value:.2f}$')
-
-        ax.set_xlim(np.min(x_limts), np.max(x_limts))
-        ax.set_ylim(np.min(y_limts), np.max(y_limts))
-        sc = ax.scatter(x_plot+z[:, 0], y_plot+z[:, 1])
-        ax.scatter(np.unique(x_plot), w_ad[::int(n_beam/n_space), frame])
-
-        return ax
-
-    n_frames = len(t_raw)
-    ani = FuncAnimation(fig, update, frames=n_frames,
-                        fargs=(x, y, x_mid, y_mid, nx, w_ad, x_plot, y_plot, t_raw, t_shaped, pinn, ax), interval=100, blit=False)
-
-    file = f'{path}/sol_time_comparison.gif'
-    ani.save(file, fps=5)
-
-
-def plot_compliance(pinn: PINN, x: torch.tensor, y: torch.tensor,
-                    t: torch.Tensor, w_ad: np.ndarray, path: str, device):
-
-    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10, 8))
-    fig.suptitle('Compliance')
-
-    x = x.to(device)
-    y = y.to(device)
-
-    t_raw = torch.unique(t, sorted=True)
-    mean_y = []
-
-    for t in t_raw:
-        t = t*torch.ones_like(x).to(device)
-        output = f(pinn, x, y, t)
-        uy = output[:, 1].cpu().detach().numpy()
-        mean_y.append(np.mean(uy))
-
-    mean_y = np.array(mean_y)
-
-    ax[0].plot(t_raw.cpu().detach().numpy(), mean_y, color='blue')
-    ax[0].set_title('Prediction from PINN')
-    ax[0].set_xlabel('$\\hat{t}$')
-    ax[0].set_ylabel('$\\overline{u}_y$')
-
-    ax[1].plot(t_raw.cpu().detach().numpy(), mean_y - np.mean(w_ad, axis=0), color='red')
-    ax[1].set_title('Deviation from analytical')
-    ax[1].set_xlabel('$\\hat{t}$')
-    ax[1].set_ylabel('$\\overline{u}_{y,an}-\\overline{u}_{y,PINN}$')
-
-    plt.grid()
-    plt.tight_layout()
-
-    file = f'{path}/compliance.png'
-    plt.savefig(file)
 
 
 def plot_energy(t: np.ndarray, V: np.ndarray, T: np.ndarray, epoch: int, path: str):
