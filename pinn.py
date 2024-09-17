1dbc9004fd49941e3afaf18319393f5bc71d7c5f
tiapia00 <10680648@polimi.it>
2024-09-17 10:41:06 +0200

    detached dV and dT

--- pinn.py
+++ pinn.py
@@ -450,14 +450,12 @@ class Loss:
                 (dyx_yy2ux[:,0] + dyx_yy2uy[:,1]) - self.adim[2] * ay.squeeze()).pow(2).mean()
         
         eps = torch.stack([dxyux[:,0], 1/2*(dxyux[:,1]+dxyuy[:,0]), dxyuy[:,1]], dim=1)
-        dV = (self.par['w0']/self.par['Lx'])**2*(self.par['mu']*torch.sum(eps**2, dim=1)) + self.par['lam']/2 * torch.sum(eps, dim=1)**2
-        dV = dV.detach()
+        dV = ((self.par['w0']/self.par['Lx'])**2*(self.par['mu']*torch.sum(eps**2, dim=1)) + self.par['lam']/2 * torch.sum(eps, dim=1)**2).detach()
 
         v = torch.cat([vx, vy], dim=1)
         vnorm = torch.norm(v, dim=1)
-        dT = 1/2*(self.par['w0']/self.par['t_ast'])**2*self.par['rho']*vnorm**2
+        dT = (1/2*(self.par['w0']/self.par['t_ast'])**2*self.par['rho']*vnorm**2).detach()
         dT *= torch.max(dV)/torch.max(dT)
-        dT = dT.detach()
 
         tgrid = torch.unique(t, sorted=True)
 
