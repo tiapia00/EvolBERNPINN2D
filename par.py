ad0796b2e2308018144f5a30f12afca473ccfc3c
tiapia00 <10680648@polimi.it>
2024-09-17 10:33:47 +0200

    decreased hiddim y

--- par.py
+++ par.py
@@ -3,9 +3,9 @@ class Parameters:
         self.x_end = 500 
         self.y_end = 0.5
         self.t_end = 0.8
-        self.n_space = 31 
+        self.n_space = 41 
         self.n_time = 43 
-        self.dim_hidden = (1, 6, 2) 
+        self.dim_hidden = (1, 3, 3) 
         self.n_hidden : int = 3 
         self.w0 = 30
         self.pinn_par = {
