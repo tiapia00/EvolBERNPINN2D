b7dab694edf58ff99a7f170f8b3b0bc51e7fe19b
tiapia00 <10680648@polimi.it>
2024-09-20 12:22:30 +0200

    increased epochs for training

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ ax.set_zlabel('Z Axis')
 #plt.show()
 plt.close()
 
-nn_inbcs = train_inbcs(nn_inbcs, loss_fn, 50, 1e-3)
+nn_inbcs = train_inbcs(nn_inbcs, loss_fn, 10000, 1e-3)
 
 x, y, t_in = points['initial_points']
 x = x.to(device)
