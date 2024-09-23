0ce0e2cdabdb9cb032048fd172e2ed3922903fc5
tiapia00 <10680648@polimi.it>
2024-09-23 17:39:26 +0200

    fixed in loss pos

--- pinn.py
+++ pinn.py
@@ -450,7 +450,7 @@ class Loss:
         space = torch.cat([x, y], dim=1)
         output = pinn(space, t)
 
-        loss = (output - 1/self.w0*initial_conditions[:,:2]).pow(2).mean(dim=0).sum()
+        loss = (output - 1/self.w0*initial_conditions(space, self.w0)[:,:2]).pow(2).mean(dim=0).sum()
 
         initial_speed = initial_conditions(space, pinn.w0)[:,2:]
         vx = torch.autograd.grad(output[:,0].unsqueeze(1), t, torch.ones_like(t, device=self.device),
