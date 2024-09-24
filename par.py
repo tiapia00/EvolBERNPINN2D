4ad68c68f356a8b825d8c0f5249b8223cb4a8e1c
tiapia00 <10680648@polimi.it>
2024-09-24 10:05:34 +0200

    decreased epochs

--- par.py
+++ par.py
@@ -22,7 +22,7 @@ class Parameters:
             'multux': self.multux,
             'multuy': self.multuy,
             'lr': 5e-4,
-            'epochs': 8000 
+            'epochs': 4000 
         }
         self.beam_par = {
             'x_end': self.x_end,
