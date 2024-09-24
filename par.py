411cf1b162478f5e9fab9c3b223d0e35a14f2ea5
tiapia00 <10680648@polimi.it>
2024-09-24 09:06:11 +0200

    increased epochs

--- par.py
+++ par.py
@@ -22,7 +22,7 @@ class Parameters:
             'multux': self.multux,
             'multuy': self.multuy,
             'lr': 1e-3,
-            'epochs': 1000 
+            'epochs': 10000 
         }
         self.beam_par = {
             'x_end': self.x_end,
