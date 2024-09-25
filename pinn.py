a9e48e19efecfbbb33e8a9ccbb3aeab5b904d0f6
tiapia00 <10680648@polimi.it>
2024-09-25 16:06:17 +0200

    reintroduced init

--- pinn.py
+++ pinn.py
@@ -433,7 +433,7 @@ class Loss:
         space = torch.cat([x, y], dim=1)
         output = pinn(space, t)
 
-        #init = initial_conditions(space, pinn.w0)
+        init = initial_conditions(space, pinn.w0)
         #loss = torch.abs(self.par['w0'] * output - init[:,:2]).mean(dim=0).sum()
         vx = torch.autograd.grad(output[:,0].unsqueeze(1), t, torch.ones_like(t, device=self.device),
                 create_graph=True, retain_graph=True)[0]
