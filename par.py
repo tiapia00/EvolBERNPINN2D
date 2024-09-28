2354695e53e9241604a3d8085528d23bf446b266
tiapia00 <10680648@polimi.it>
2024-09-28 12:41:48 +0200

    increased hiddim

--- par.py
+++ par.py
@@ -5,7 +5,7 @@ class Parameters:
         self.t_end = 1 
         self.n_space = 40 
         self.n_time = 40 
-        self.dim_hidden = 50
+        self.dim_hidden = 70
         self.n_hidden : int = 3 
         self.w0 = 1 
         self.pinn_par = {
