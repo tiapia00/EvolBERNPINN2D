5ea05ad458f9dfeae0dc2f98fd72160a61267e7a
tiapia00 <10680648@polimi.it>
2024-09-29 18:56:02 +0200

    corrected mode initialization

--- pinn.py
+++ pinn.py
@@ -243,9 +243,9 @@ class PINN(nn.Module):
         #self.By[0,:] = torch.ones([1, n_mode_spacey], device=device) * torch.arange(n_mode_spacey, device=device)
         
         self.Btx = torch.ones((1, n_mode_spacex), device=device)
-        self.By[0, self.Btx.shape[1] // 2:] *= 2
         self.btx = torch.randn(1, n_mode_spacex, device=device)
         self.Bty = torch.ones(1, n_mode_spacey, device=device) 
+        self.Bty[0,self.Btx.shape[1] // 2:] *= 2
         self.bty = torch.randn(1, n_mode_spacey, device=device)
 
         self.hid_space_layers_x = nn.ModuleList()
