634c3c4ae860f6eedb5b2fbb86406634efac057b
tiapia00 <10680648@polimi.it>
2024-09-29 11:09:45 +0200

    decreased lr

--- par.py
+++ par.py
@@ -17,7 +17,7 @@ class Parameters:
             'w0': self.w0,
             'dim_hidden': self.dim_hidden,
             'n_hidden_space': self.n_hidden,
-            'lr': 1e-3,
+            'lr': 1e-4,
             'epochs': 10000 
         }
         self.beam_par = {
