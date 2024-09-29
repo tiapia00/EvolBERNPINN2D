a57cd8b4f289c12c2ac9fb8f8cf3d974624ad52f
tiapia00 <10680648@polimi.it>
2024-09-29 15:51:13 +0200

    modified par

--- par.py
+++ par.py
@@ -5,8 +5,8 @@ class Parameters:
         self.t_end = 1 
         self.n_space = 40 
         self.n_time = 40 
-        self.dim_hidden = 50
-        self.n_hidden : int = 1 
+        self.dim_hidden = 40
+        self.n_hidden : int = 2 
         self.multhyperx = 2
         self.w0 = 1 
         self.pinn_par = {
