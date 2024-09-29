f9c73b0e585b799930fedec4d73e35ba75dd4d27
tiapia00 <10680648@polimi.it>
2024-09-29 19:00:16 +0200

    decreased t end

--- par.py
+++ par.py
@@ -2,7 +2,7 @@ class Parameters:
     def __init__(self):
         self.x_end = 500 
         self.y_end = 0.5
-        self.t_end = 1 
+        self.t_end = 0.5 
         self.n_space = 50
         self.n_time = 40 
         self.dim_hidden = (1, 10) 
