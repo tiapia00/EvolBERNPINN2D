c747f1455280f8ca3b892f51d9d865ca2c9b062e
tiapia00 <10680648@polimi.it>
2024-09-29 10:44:38 +0200

    increased dim hidden

--- pinn.py
+++ pinn.py
@@ -183,47 +183,6 @@ class Grid:
         return (x_all, y_all, t_all)
 
 
-class RBF(nn.Module):
-    def __init__(self, in_features, out_features, basis_func):
-        super(RBF, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.centres = nn.Parameter(torch.Tensor(out_features, in_features))
-        self.log_sigmas = nn.Parameter(torch.Tensor(out_features))
-        self.basis_func = basis_func
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        nn.init.normal_(self.centres, 0, 1)
-        nn.init.constant_(self.log_sigmas, 0)
-
-    def forward(self, input):
-        size = (input.size(0), self.out_features, self.in_features)
-        x = input.unsqueeze(1).expand(size)
-        c = self.centres.unsqueeze(0).expand(size)
-        distances = (x - c).pow(2).sum(-1).pow(0.5) / \
-            torch.exp(self.log_sigmas).unsqueeze(0)
-        return self.basis_func(distances)
-
-def inverse_multiquadric(alpha):
-    phi = torch.ones_like(alpha) / (torch.ones_like(alpha) + alpha.pow(2)).pow(0.5)
-    return phi
-
-def gaussian(alpha):
-    phi = torch.exp(-1*alpha.pow(2))
-    return phi
-
-def matern52(alpha):
-    phi = (torch.ones_like(alpha) + 5 ** 0.5 * alpha + (5 / 3) * alpha.pow(2)) * torch.exp(-5 ** 0.5 * alpha)
-    return phi
-
-class TrigAct(nn.Module):
-    def forward(self, x):
-        return torch.sin(x)
-
-def parabolic(a, x):
-    return (a * x ** 2 - a * x)
-
 class PINN(nn.Module):
     def __init__(self,
                  hiddendim: int,
