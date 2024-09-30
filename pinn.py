e53d13e81875c7b5a38a93f2247f22bed8a44d0c
tiapia00 <10680648@polimi.it>
2024-09-30 15:09:24 +0200

    applied orthogonal initialization

--- pinn.py
+++ pinn.py
@@ -284,12 +284,12 @@ class PINN(nn.Module):
             param.requires_grad = False
 
         self.initlayer = nn.Linear(3, 2*hiddendim, bias=False)
-        #nn.init.xavier_normal_(self.initlayer.weight)
+        nn.init.orthogonal_(self.initlayer.weight)
 
         self.layers = nn.ModuleList([])
         for _ in range(nhidden):
             self.layers.append(nn.Linear(2*hiddendim, 2*hiddendim, bias=False))
-            #nn.init.xavier_normal_(self.layers[-1].weight)
+            nn.init.orthogonal_(self.layers[-1].weight)
         
         self.outlayerx = nn.Linear(2*hiddendim, 1, bias=False)
         self.outlayerx.weight.data *= 0
