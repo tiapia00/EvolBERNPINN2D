c412eff41a5121c45ba96677855452dcac7442fa
tiapia00 <10680648@polimi.it>
2024-10-07 15:27:04 +0200

    modified par

--- par.py
+++ par.py
@@ -5,7 +5,7 @@ class Parameters:
         self.t_end = 1 
         self.n_space = 50
         self.n_time = 40 
-        self.dim_hidden = (1, 10) 
+        self.dim_hidden = (1, 3) 
         self.multux = 1
         self.multuy = 1 
         self.multhyperx = 2 
