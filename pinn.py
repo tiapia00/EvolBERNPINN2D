2d6d008f82a56d7b5cd98d67653d6712c4d6ba00
tiapia00 <10680648@polimi.it>
2024-10-09 17:01:59 +0200

    classic res_loss

--- pinn.py
+++ pinn.py
@@ -392,22 +392,11 @@ class Loss:
         loss += (self.adim[0] * (dxx_xy2uy[:,0] + dyx_yy2uy[:,1]) + self.adim[1] * 
                 (dyx_yy2ux[:,0] + dyx_yy2uy[:,1]) - self.adim[2] * ay.squeeze()).pow(2).mean()
         
-        loss *= self.penalty[0].item()
+        loss *= self.adaptive[0].item()
         
-        H = torch.zeros(self.n_space, self.n_space, self.n_time, 2, 2, device=self.device)
-        dxyux = dxyux.reshape(self.n_space, self.n_space, self.n_time, 2)
-        dxyuy = dxyuy.reshape(self.n_space, self.n_space, self.n_time, 2)
-        # last 2 is related to the the components of the derivative
-        # nx, ny, nt, 2, 2
-        H[:, :, :, 0, :] = dxyux
-        H[:, :, :, 1, :] = dxyuy
-        eps = H
-        eps[:, :, :, [0, 1], [1, 0]] = 0.5 * (eps[:, :, :, 0, 1] + 
-                eps[:, :, :, 1, 0]).unsqueeze(3).expand(-1,-1,-1,2)
-        
-        sig, dV = material_model(eps, self.par, self.device)
-        dV = dV.reshape(self.n_space**2*self.n_time)
-        
+        eps = torch.stack([dxyux[:,0], 1/2*(dxyux[:,1]+dxyuy[:,0]), dxyuy[:,1]], dim=1)
+        dV = ((self.par['w0']/self.par['Lx'])**2*(self.par['mu']*torch.sum(eps**2, dim=1)) + self.par['lam']/2 * torch.sum(eps, dim=1)**2)
+
         v = torch.cat([vx, vy], dim=1)
         vnorm = torch.norm(v, dim=1)
         dT = (1/2*(self.par['w0']/self.par['t_ast'])**2*self.par['rho']*vnorm**2)
@@ -417,48 +406,15 @@ class Loss:
 
         V = torch.zeros(tgrid.shape[0])
         T = torch.zeros_like(V)
-        W_ext_eff = torch.zeros_like(V)
-        W_ext_an = torch.zeros_like(V)
-
-        # Boundterm
-        _, _, left, _, _ = self.points['boundary_points']
-        points = torch.cat([space, t], dim=1)
-
-        matches = (points.unsqueeze(1) == left.unsqueeze(0)).all(dim=-1)
-        neumannidx = torch.nonzero(matches)[:, 0]
-
-        sig = sig.reshape(self.n_space**2*self.n_time, 4)
-        tractionleft = sig[:,-1][neumannidx]
-        prescribed = torch.exp(
-                - points[neumannidx,-1].unsqueeze(1)) * torch.sin(2 * np.pi * points[neumannidx,-1].unsqueeze(1))
-        # MPa
-
-        lossN = (tractionleft - prescribed).pow(2).mean()
-        prescribed = prescribed.reshape(self.n_space, self.n_time - 1)
-        tractionleft = tractionleft.reshape(self.n_space, self.n_time - 1)
-
         for i, ts in enumerate(tgrid):
             tidx = torch.nonzero(t.squeeze() == ts).squeeze()
             dVt = dV[tidx].reshape(self.n_space, self.n_space)
             dTt = dT[tidx].reshape(self.n_space, self.n_space)
-            if i != 0:
-                tidxN = torch.nonzero(left[:,-1] == ts).squeeze()
-                uyneut = output[tidxN, -1].reshape(self.n_space)
-                dWext = tractionleft[:, i-1] * uyneut
-                dWext = torch.max(dVt)/torch.max(dWext) * dWext
-                W_ext_eff[i] = self.b * simps(dWext, self.steps[0])
-                dWextan = prescribed[:, i-1] * uyneut
-                dWextan = torch.max(dVt)/torch.max(dWextan) * dWextan
-                dWextan = dWextan.detach()
-                W_ext_an[i] = self.b * simps(dWextan, self.steps[0])
-            else:
-                W_ext_eff[i] = 0
-                W_ext_an[i] = 0
 
             V[i] = self.b*simps(simps(dVt, self.steps[1]), self.steps[0])
             T[i] = self.b*simps(simps(dTt, self.steps[1]), self.steps[0])
 
-        return loss, V, T, W_ext_eff, W_ext_an, lossN.detach()
+        return loss, V, T
 
     def bound_N_loss(self, pinn):
         _, _, left, right, _ = self.points['boundary_points']
@@ -505,11 +461,11 @@ class Loss:
         return loss, (losspos, lossv)
 
     def verbose(self, pinn):
-        res_loss, V, T, Wext_eff, Wext_an, lossN = self.res_loss(pinn)
-        enloss = ((V[0] + T[0] + Wext_eff[0]) - (Wext_eff + V + T)).pow(2).mean()
+        res_loss, V, T = self.res_loss(pinn)
+        enloss = ((V[0] + T[0]) - (V + T)).pow(2).mean()
         boundloss = self.bound_N_loss(pinn)
         init_loss, init_losses = self.initial_loss(pinn)
-        loss = init_loss
+        loss = res_loss + init_loss
 
         losses = {
             "in_losses": init_losses,
