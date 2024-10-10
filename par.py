5f9614acf09d24a2a1530f8947caf85429e23904
tiapia00 <10680648@polimi.it>
2024-10-10 12:25:14 +0200

    increased lr

--- par.py
+++ par.py
@@ -23,7 +23,7 @@ class Parameters:
             'multux': self.multux,
             'multuy': self.multuy,
             'multhyperx': self.multhyperx,
-            'lr': 5e-5,
+            'lr': 1e-4,
             'epochs': 10000 
         }
         self.beam_par = {
