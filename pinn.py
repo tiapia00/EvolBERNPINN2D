9c73fbaa18a56c514d589b178c41067649cca3f7
tiapia00 <10680648@polimi.it>
2024-10-10 09:04:02 +0200

    reintroduced adim

--- pinn.py
+++ pinn.py
@@ -486,7 +486,7 @@ class Loss:
         
         v = torch.cat([vx, vy], dim=1)
 
-        lossv = self.penalty[2].item() * (v * self.par['w0']- init[:,2:]).pow(2).mean(dim=0).sum()
+        lossv = self.penalty[2].item() * (v * self.par['w0']/self.par['t_ast']- init[:,2:]).pow(2).mean(dim=0).sum()
 
         loss = lossv + losspos
 
