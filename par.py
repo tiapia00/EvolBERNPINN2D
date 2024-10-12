3431fb20631bd778f7c28e8e320944c167c131e0
tiapia00 <10680648@polimi.it>
2024-10-12 12:44:34 +0200

    decreased lr

--- par.py
+++ par.py
@@ -23,7 +23,7 @@ class Parameters:
             'multux': self.multux,
             'multuy': self.multuy,
             'multhyperx': self.multhyperx,
-            'lr': 1e-3,
+            'lr': 1e-4,
             'epochs': 5000 
         }
         self.beam_par = {
