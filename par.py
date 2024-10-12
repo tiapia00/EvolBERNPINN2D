e7f257aaaca2e09e9e56042d1ae5ac1fc43b19d0
tiapia00 <10680648@polimi.it>
2024-10-12 11:27:28 +0200

    increased dimhidden

--- par.py
+++ par.py
@@ -5,7 +5,7 @@ class Parameters:
         self.t_end = 0.8
         self.n_space = 40
         self.n_time = 50 
-        self.dim_hidden = (1, 30) 
+        self.dim_hidden = (1, 60) 
         self.multux = 1
         self.multuy = 1 
         self.multhyperx = 1 
