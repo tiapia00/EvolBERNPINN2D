5eb3d890a18922a98cafe547f993b42d4c5bef91
tiapia00 <10680648@polimi.it>
2024-10-13 17:48:07 +0200

    increased w0

--- par.py
+++ par.py
@@ -10,7 +10,7 @@ class Parameters:
         self.multuy = 1 
         self.multhyperx = 1 
         self.n_hidden : int = 2 
-        self.w0 = 0.07 
+        self.w0 = 0.1 
         self.pinn_par = {
             'x_end': self.x_end,
             'y_end': self.y_end,
