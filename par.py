046826aed962d481f129bd4f5da07835606499f6
Mattia Andreoli <tiapia00@gmail.com>
2024-10-17 15:59:40 +0000

    increased lr

--- par.py
+++ par.py
@@ -23,7 +23,7 @@ class Parameters:
             'multux': self.multux,
             'multuy': self.multuy,
             'multhyperx': self.multhyperx,
-            'lr': 1e-5,
+            'lr': 1e-4,
             'epochs': 7000 
         }
         self.beam_par = {
