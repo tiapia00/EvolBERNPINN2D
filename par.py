a772e83b9c0c0ab0c2eba567d458e2d75aba27e8
tiapia00 <10680648@polimi.it>
2024-10-26 17:20:59 +0200

    increased dimhiddne and epochs

--- par.py
+++ par.py
@@ -5,7 +5,7 @@ class Parameters:
         self.t_end = 0.5 
         self.n_space = 50 
         self.n_time = 50 
-        self.dim_hidden = (1, 30) 
+        self.dim_hidden = (1, 50) 
         self.multux = 1
         self.multuy = 1 
         self.multhyperx = 1 
@@ -24,7 +24,7 @@ class Parameters:
             'multuy': self.multuy,
             'multhyperx': self.multhyperx,
             'lr': 5e-4,
-            'epochs': 4000 
+            'epochs': 10000 
         }
         self.beam_par = {
             'x_end': self.x_end,
