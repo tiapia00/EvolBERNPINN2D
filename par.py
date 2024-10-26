c975d9ab14c89c21734c7ea02c1c9c71b9f33fd8
tiapia00 <10680648@polimi.it>
2024-10-26 16:54:11 +0200

    reduced dimhidden

--- par.py
+++ par.py
@@ -4,8 +4,8 @@ class Parameters:
         self.y_end = 0.5
         self.t_end = 0.5 
         self.n_space = 50 
-        self.n_time = 45 
-        self.dim_hidden = (1, 60) 
+        self.n_time = 50 
+        self.dim_hidden = (1, 30) 
         self.multux = 1
         self.multuy = 1 
         self.multhyperx = 1 
