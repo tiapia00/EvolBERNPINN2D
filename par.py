ba3522af4cc2b2117dcbc21a41cd4d15a4d7af23
tiapia00 <10680648@polimi.it>
2024-11-08 20:05:16 +0100

    decreased epochs

--- par.py
+++ par.py
@@ -28,7 +28,7 @@ class Parameters:
             'modesx': self.modesx,
             'modesy': self.modesy,
             'lr': 1e-3,
-            'epochs': 8000 
+            'epochs': 800 
         }
         self.beam_par = {
             'x_end': self.x_end,
