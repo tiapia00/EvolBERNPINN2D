46f928b97164c90646f671c373876465277672ed
tiapia00 <10680648@polimi.it>
2024-11-08 16:25:28 +0100

    just single mode activated

--- analytical.py
+++ analytical.py
@@ -25,8 +25,8 @@ def obtain_analytical_free(my_beam: Beam, amp0: float, tf: float,
     my_beam.update_phi(phi)
     my_In_Cond = In_Cond(my_beam)
 
-    w0 = amp0 * (my_beam.phi[:,1] + 2 * my_beam.phi[:,4])
-    wdot_0 = amp0 * (my_beam.phi[:,1] + my_beam.phi[:,4])
+    w0 = amp0 * (my_beam.phi[:,1])
+    wdot_0 = amp0 * (my_beam.phi[:,1])
 
     my_In_Cond.pass_init_cond(w0, wdot_0)
     A, B = my_In_Cond.compute_coeff()
