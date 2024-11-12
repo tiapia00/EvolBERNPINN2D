d0ef2268307cc61a92040406fdfbf3219893032c
tiapia00 <10680648@polimi.it>
2024-11-12 08:49:06 +0100

    added limits

--- analytical.py
+++ analytical.py
@@ -25,7 +25,7 @@ def obtain_analytical_free(my_beam: Beam, amp0: float, tf: float,
     my_beam.update_phi(phi)
     my_In_Cond = In_Cond(my_beam)
 
-    w0 = amp0 * (my_beam.phi[:,1] + 2 * my_beam.phi[:,7])
+    w0 = amp0 * (my_beam.phi[:,1])
     wdot_0 = -10 * amp0 * (my_beam.phi[:,1] + my_beam.phi[:,4])
 
     my_In_Cond.pass_init_cond(w0, wdot_0)
