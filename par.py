283d9d079bf20bef466ee5705f55ec278961a7e0
tiapia00 <10680648@polimi.it>
2024-11-16 16:58:21 +0100

    modified pars

--- par.py
+++ par.py
@@ -7,7 +7,7 @@ class Parameters:
         self.n_time = 50 
         self.dim_hidden = (1, 60) 
         self.modesx = 1
-        self.modesy = [1.8, 4.7]
+        self.modesy = [1.8, 7] 
         self.multux = 1
         self.multuy = 1 
         self.multhyperx = 3 
